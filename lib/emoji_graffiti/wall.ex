2fd2905495c7941c6b918b360b12ca5f6f481dbf
praguevara <praguevara@gmail.com>
2024-08-21 18:53:47 +0200

    Improve queries to db

--- lib/emoji_graffiti/wall.ex
+++ lib/emoji_graffiti/wall.ex
@@ -42,16 +42,18 @@ defmodule EmojiGraffiti.Wall do
 
   @impl true
   def handle_call({:get_many, from, to}, _from, state) do
-    range = from..to
-    {cells, new_state} = get_or_fetch_cells(range, state)
+    {cells, new_state} = get_or_fetch_cells(from, to, state)
     result = Enum.map(from..to, fn id -> cells[id] end)
     {:reply, result, new_state}
   end
 
   @impl true
   def handle_call({:update, id, emoji}, _from, state) do
-    cell = Map.get(state, id) || %Cell{id: id}
+    cell = %Cell{id: id}
     updated_cell = %{cell | emoji: emoji}
+
+    new_state = Map.put(state, id, updated_cell)
+    persist_to_db(updated_cell)
 
     Phoenix.PubSub.broadcast(
       EmojiGraffiti.PubSub,
@@ -59,8 +61,6 @@ defmodule EmojiGraffiti.Wall do
       {:emoji_changed, id, emoji}
     )
 
-    new_state = Map.put(state, id, updated_cell)
-    persist_to_db(updated_cell)
     {:reply, {:ok, updated_cell}, new_state}
   end
 
@@ -80,28 +80,31 @@ defmodule EmojiGraffiti.Wall do
     end
   end
 
-  defp get_or_fetch_cells(ids, state) do
-    {present, missing} = Enum.split_with(ids, &Map.has_key?(state, &1))
-    present_cells = Map.take(state, present)
-    fetched_cells = fetch_many_from_db(missing)
-    cells = Map.merge(present_cells, fetched_cells)
-    new_state = Map.merge(state, fetched_cells)
-    {cells, new_state}
+  defp get_or_fetch_cells(from, to, state) do
+    all_present = Enum.all?(Enum.to_list(from..to), &Map.has_key?(state, &1))
+
+    if all_present do
+      {Map.take(state, Enum.to_list(from..to)), state}
+    else
+      cells = fetch_range_from_db(from, to)
+      new_state = Map.merge(state, cells)
+      {cells, new_state}
+    end
   end
 
   defp fetch_from_db(id) do
     Repo.get(Cell, id) || %Cell{id: id, emoji: default_emoji()}
   end
 
-  defp fetch_many_from_db(ids) do
+  defp fetch_range_from_db(from, to) do
     cells =
       Cell
-      |> where([c], c.id in ^ids)
+      |> where([c], c.id >= ^from and c.id <= ^to)
       |> Repo.all()
 
     existing_cells = Map.new(cells, fn cell -> {cell.id, cell} end)
 
-    Enum.reduce(ids, existing_cells, fn id, acc ->
+    Enum.reduce(from..to, existing_cells, fn id, acc ->
       Map.put_new(acc, id, %Cell{id: id, emoji: default_emoji()})
     end)
   end
